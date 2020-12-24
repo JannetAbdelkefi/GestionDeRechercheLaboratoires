79753bff542e2caa4114546ecdb88129789d1d7f
jannet <abdelkefi.jannet@gmail.com>
2020-12-24 01:18:42 +0100

    add flex layout

--- src/app/@root/shared/shared.module.ts
+++ src/app/@root/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import {MaterialModule} from '../material/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 
 @NgModule({
   imports: [
     MaterialModule,
+    FlexLayoutModule,
   ],
   exports: [
     MaterialModule,
+    FlexLayoutModule,
   ]
 })
 export class SharedModule { }
